85eebe0b45f91cc4161d556683403b5d08538c95
MinaMamdouh2 <sxgs555@gmail.com>
2023-11-26 19:28:16 +0200

    Added testgrp handler

---
+++ app/services/chat-api/handlers/v1/tstgrp/tstgrp.go
@@ -0,0 +1,24 @@
+package testgrp
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/MinaMamdouh2/Chat-Application-Backend/foundation/web"
+)
+
+// Test is our example route
+func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	// Vlidate the data
+	// Call into theBusiness layer
+	// Return errors
+	//Handle OK responses
+	status := struct {
+		Status string
+	}{
+		Status: "OK",
+	}
+	// This should be used for the OK responses only.
+	return web.Respond(ctx, w, status, http.StatusOK)
+}
