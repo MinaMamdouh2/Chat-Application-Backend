32b35ca78533e3eaf7e15cf90e18ecb9d2889903
MinaMamdouh2 <sxgs555@gmail.com>
2023-11-26 19:28:00 +0200

    Added main entry point

---
+++ app/services/chat-api/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"errors"
+	"net/http"
+
+	"github.com/MinaMamdouh2/Chat-Application-Backend/app/services/chat-api/handlers"
+	"github.com/MinaMamdouh2/Chat-Application-Backend/buisness/web/v1/debug"
+	"github.com/MinaMamdouh2/Chat-Application-Backend/foundation/logger"
+	"github.com/ardanlabs/conf/v3"
+	"go.uber.org/zap"
+)
+
+var build = "develop"
+
+func main() {
+	log, err := logger.New("CHAT-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	ctx := context.Background()
+
+	if err := run(log, ctx); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+/*
+	//TODO:
+	Need to figure out timeouts for http service.
+	Add Category field and type to product.
+*/
+
+func run(log *zap.SugaredLogger, ctx context.Context) error {
+	// -----------------------------------------------------------------------
+	// GOMAXPROCS
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "BUILD-", build)
+
+	// -------------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000,mask"` //mask print it as xxxxxx
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+		Auth struct {
+			KeysFolder string `conf:"default:zarf/keys/"`
+			ActiveKID  string `conf:"default:private"`
+			Issuer     string `conf:"default:chat service"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Mina Mamdouh",
+		},
+	}
+
+	const prefix = "CHAT"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info("starting service ", "version ", build)
+	defer log.Info("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+
+	log.Info("startup", "config", out)
+
+	// Simple keystore.
+	// ks, err := keystore.NewFS(os.DirFS(cfg.Auth.KeysFolder))
+	// if err != nil {
+	// 	return fmt.Errorf("reading keys: %w", err)
+	// }
+
+	if err != nil {
+		return fmt.Errorf("constructing auth: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// Start Debug Service
+	// This creats a go that blocks on a listening serve call on whatever the IP for the debug host is
+
+	go func() {
+		if err := http.ListenAndServe(cfg.Web.DebugHost, debug.StandardLibraryMux()); err != nil {
+			log.Error("shutdown", "status", "debug router closed", "host", cfg.Web.DebugHost, "msg", err)
+		}
+	}()
+
+	// -----------------------------------------------------------------------
+	// Start API Service
+	log.Infow("startup", "status", "Initializing V1 API support")
+
+	serverErrors := make(chan error, 1)
+
+	// -----------------------------------------------------------------------
+	shutdown := make(chan os.Signal, 1)
+
+	apiMux := handlers.APIMux(handlers.APIMuxConfig{
+		Shutdown: shutdown,
+		Log:      log,
+	})
+
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		Handler:      apiMux,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     zap.NewStdLog(log.Desugar()),
+	}
+
+	go func() {
+		log.Infow("startup", "status", "api router started", "host", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+	// we are waiting for SIGINT which is a Ctrl+C or
+	// a SIGTERM which what will get back from Kubernetes
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(ctx, cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		if err := api.Shutdown(ctx); err != nil {
+			// If we timeo
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
+
+	return nil
+}
